e3edb4ed41cf59ffbca83de608542f80b2f0431d
LeanGnzlz <lean03gonzalez@gmail.com>
2026-06-16 17:44:03 -0300

    Added first port scanning functionality

--- scanner.py
+++ scanner.py
@@ -1,1 +1,24 @@
-print("Network Scanner iniciado")
+import socket
+
+print("================================")
+print("      Network Scanner")
+print("================================")
+
+target = input("Ingrese la IP objetivo: ")
+
+print(f"Escaneando: {target}")
+
+def scan_port(target, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    result = sock.connect_ex((target, port))
+
+    if result == 0:
+        print(f"Puerto {port} abierto")
+    else:
+        print(f"Puerto {port} cerrado")
+
+    sock.close()
+
+
+scan_port(target, 80)
