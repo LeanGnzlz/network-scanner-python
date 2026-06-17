cdcea985561b704f072e4af92105163e2af1689c
LeanGnzlz <lean03gonzalez@gmail.com>
2026-06-17 15:43:43 -0300

    Added timeout and extended port list

--- scanner.py
+++ scanner.py
@@ -8,11 +8,13 @@ target = input("Ingrese la IP objetivo: ")
 
 print(f"Escaneando: {target}")
 
-ports = [21, 22, 23, 80, 443]
+ports = [21, 22, 23, 25, 53, 80, 110, 139, 143, 443, 445, 3389]
 
 def scan_port(target, port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sock.settimeout(1)
 
         result = sock.connect_ex((target, port))
 
